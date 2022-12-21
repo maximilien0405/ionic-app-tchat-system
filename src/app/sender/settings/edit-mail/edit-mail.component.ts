ad03b08915097baac9f812fa64ecffa7ebd1c263
maximilien0405 <maximilien.zimmermann@ik.me>
2022-12-21 18:04:59 +0100

    feat(account): edit-mail done

--- src/app/sender/settings/edit-mail/edit-mail.component.ts
+++ src/app/sender/settings/edit-mail/edit-mail.component.ts
@@ -3,19 +3,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
 import { Preferences } from '@capacitor/preferences';
 import { NavController } from '@ionic/angular';
+import { Subject } from 'rxjs';
+import { slideRightAnimation } from 'src/app/common/animations';
 import { GetUserService } from 'src/app/common/services/get-user.service';
 import { UserService } from 'src/app/common/services/user.service';
 
 @Component({
   selector: 'app-edit-mail',
   templateUrl: './edit-mail.component.html',
-  styleUrls: ['../../sender.component.scss']
+  styleUrls: ['../../sender.component.scss'],
+  animations: [slideRightAnimation]
 })
 export class EditMailComponent implements OnInit {
   public form1: FormGroup;
   public form2: FormGroup;
   public spinnerDisplay: boolean;
   public step: number = 1;
+  public errorCode: boolean;
+  public term = new Subject<string>();
+  public submittedForm: boolean;
 
   constructor(private formBuilder: FormBuilder, 
     private navCtrl: NavController, 
@@ -30,13 +36,40 @@ export class EditMailComponent implements OnInit {
     })
 
     this.form2 = this.formBuilder.group({
-      code: ['', [Validators.minLength(5), Validators.maxLength(5), Validators.required]],
+      code: ['', [Validators.required]],
+    })
+
+    // Listen on code value
+    this.form2.get('code')?.valueChanges.subscribe((selectedValue: any) => {
+      if (!selectedValue) {
+        this.errorCode = false;
+      }
     })
   }
 
   // Return form controls
   get f1() { return this.form1.controls; }
   get f2() { return this.form2.controls; }
+
+  // On code change execute
+  public onCodeChange(code: string): void {
+    if (code == "") {
+      this.errorCode = false;
+      return;
+    }
+    if (code.length == 5) {
+      this.term.next(code);
+    }
+  }
+
+  // Only number on code field
+  public numberOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
 
   // Ask for a code
   public submitForm1(): void {
@@ -61,6 +94,7 @@ export class EditMailComponent implements OnInit {
 
   // Validate the code and change email
   public submitForm2(): void {
+    this.submittedForm = true;
     Haptics.impact({ style: ImpactStyle.Medium });
     this.spinnerDisplay = true;
 
@@ -70,6 +104,7 @@ export class EditMailComponent implements OnInit {
     })
     .then((res) => {
       if (res.error) {
+        this.errorCode = true;
         this.spinnerDisplay = false;
       } else {
         const changeMail = async () => {
