0a9b21cb25bf84e91b9ec402fd49df2953a32c40
maximilien0405 <maximilien.zimmermann@ik.me>
2023-02-19 17:12:52 +0100

    fix(socket): fix socket token bug mobile

--- src/app/common/services/tchat-socket.service.ts
+++ src/app/common/services/tchat-socket.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 import { SocketIoConfig, Socket } from 'ngx-socket-io';
 import { environment } from 'src/environments/environment';
 
 const config: SocketIoConfig = {
-  url: environment.apiUrl,
-  options: {
-    transportOptions: {
-      polling: {
-        extraHeaders: {
-          Authorization: localStorage.getItem('CapacitorStorage.token')
-        },
-      },
-    },
-  }
+  url: environment.apiUrl
 };
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class TchatSocketService extends Socket {
-
   constructor() {
-    super(config)
+    super(config);
+
+    const getToken = async () => {
+      const { value } = await Preferences.get({ key: 'token' });
+      if (value) {
+        console.log(value)
+        this.ioSocket['auth'] = { token: value };
+      }
+    }; getToken()
   }
 }
